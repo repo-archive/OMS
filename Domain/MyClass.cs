afb17ff70cf552ebb153cc66310d24f0c52fd4cc
stevie <yeowtang@gmail.com>
2018-12-07 00:37:16 +0000

    Initial

---
+++ Domain/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Domain
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ Infrastructure/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Infrastructure
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
