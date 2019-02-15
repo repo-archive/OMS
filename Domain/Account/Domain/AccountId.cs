b613bb3113e41d3993587d410f36488c09528994
Steven <yeowtang@yahoo.com.au>
2019-02-15 00:32:37 +0000

    getting there with account

---
+++ Domain/Account/Domain/AccountId.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Account.Domain
+{
+    public class AccountId : Identity
+
+    {
+        public AccountId(string id)
+        {
+            Id = id;
+        }
+    }
+}
